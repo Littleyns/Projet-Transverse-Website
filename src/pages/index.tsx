9076b8ba2ab18685feb765f317f34d2baa8e75d9
Youness Srhir <Ysrhir@hotmail.fr>
2021-05-07 06:12:15 +0200

    final

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,6 +22,7 @@ const IndexPage = (props: LayoutProps) =>
         <img src="https://nsa40.casimages.com/img/2021/05/06/mini_21050607171781187.png" width="80px" height="80px" id="imgg"></img>
         <Header inverted as="h1">Just Heal Integration</Header>
         <Header inverted as="h2">L'établissement des fondations !</Header>
+        <p>Notre priorité ? Faciliter vos premiers pas dans le groupe</p>
       </Container>
     </Segment>
 
@@ -50,37 +51,32 @@ Nous avons préparé un parcours d’intégration sur une semaine, composée de 
           <Grid.Column>
             <Header icon>
             <img src="https://nsa40.casimages.com/img/2021/05/06/mini_210506070514436273.png"></img>
-              <h2>A kind of magic!</h2>
+              <h2>Vincent, Consultant Technique</h2>
             </Header>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Voluptas eaque at quae cupiditate aspernatur quibusdam!
-                  Distinctio quod non, harum dolorum earum molestias,
-                  beatae expedita aliquam dolorem asperiores nemo amet quaerat.
+            Développeur Androïd Diplômé de l’ESIEA. Stage de fin d’études chez just healpuis CDI
+J’ai travaillé au sein d’une équipe projet de quatre personnes. J’ai reçu un bon accueil, l’ambiance était très sympa et mon tuteur à l’écoute. Le bilan est très positif, j’ai eu beaucoup de liberté et surtout du temps pour faire de la veille technologique. En plus j’ai pu participer à des évènements sympas (tournois de foot, soirées,Espace game…) Mon expérience en un mot : liberté
                 </p>
           </Grid.Column>
           <Grid.Column>
             <Header icon>
             <img src="https://nsa40.casimages.com/img/2021/05/06/mini_210506070514436273.png"></img>
-              <h2>A kind of magic!</h2>
+              <h2>Jean, Développeur d'application</h2>
             </Header>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Voluptas eaque at quae cupiditate aspernatur quibusdam!
-                  Distinctio quod non, harum dolorum earum molestias,
-                  beatae expedita aliquam dolorem asperiores nemo amet quaerat.
+            Entré comme stagiaire chez just heal , j’y suis depuis 2 ans maintenant et j’occupe aujourd’hui le poste de leader technique au sein d’un projet OBS d’environ 8 personnes. »
+Un accueil personnalisé dès votre premier jour 
+ 
+A votre arrivée, vous êtes accueilli par votre manager. Accessible et attentif, il vous présente votre projet et votre environnement de travail. Vous rencontrerez les équipes avec lesquelles vous travaillerez et serez accompagné dans vos premiers pas sur votre projet.
                 </p>
           </Grid.Column>
           <Grid.Column>
             <Header icon>
               <img src="https://nsa40.casimages.com/img/2021/05/06/mini_210506070514436273.png"></img>
-              <h2>A kind of magic!</h2>
+              <h2>Charline, Chargée de communication</h2>
             </Header>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Voluptas eaque at quae cupiditate aspernatur quibusdam!
-                  Distinctio quod non, harum dolorum earum molestias,
-                  beatae expedita aliquam dolorem asperiores nemo amet quaerat.
+            Ce qui m’a frappée lors de mon arrivée, ce sont les nombreuses attentions dont nous sommes les bénéficiaires permettant aussi de vivre une intégration réussie : cadeaux de bienvenue, visite des locaux, etc. Un exemple ? La première ouverture de ma boîte mail. Lorsque les mails ont tous commencés à apparaître et que j’ai pu voir tous les événements auxquels j’étais déjà conviée : afterwork de la BU, tournoi de e-sport, séminaire marque employeur, … C’est plaisant de découvrir que l’on fait déjà entièrement partie de l'entreprise avant même son arrivée ! 
                 </p>
           </Grid.Column>
         </Grid.Row>
